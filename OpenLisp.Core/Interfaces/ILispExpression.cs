ad9fb0838b967d9b269b23e032be0a578cb93c37
Ramon Long <ramonjlongiii@gmail.com>
2015-08-16 18:32:52 -0400

    Initial commit.

---
+++ OpenLisp.Core/Interfaces/ILispExpression.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OpenLisp.Core.Interfaces
+{
+    public interface ILispExpression<T>
+    {
+        IList<ILispSymbol<T>> LispExpression { get; set; }
+    }
+}
---
+++ OpenLisp.Core/Interfaces/ILispSymbol.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OpenLisp.Core.Interfaces
+{
+    public interface ILispSymbol<T>
+    {
+        string OpenLispSymbol { get; set; }
+
+        Func<T> OpenLispMethodFunc { get; set; }
+    }
+}
