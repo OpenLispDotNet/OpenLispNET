c643212607d561bfcc518cba77dc3dc56d57f79f
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 01:57:01 -0400

    Adding OpenLispVector class.

---
+++ OpenLisp.Core/DataTypes/OpenLispVector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispVector : OpenLispList
+    {
+        public OpenLispVector() : base()
+        {
+            Start = "[";
+
+            End = "]";
+        }
+
+        public OpenLispVector(List<OpenLispVal> val)
+            : base(val)
+        {
+            Start = "[";
+
+            End = "]";
+        }
+
+        public override bool ListQ()
+        {
+            return false;
+        }
+
+        public override OpenLispList Slice(int start, int end)
+        {
+            var value = this.Value;
+
+            return new OpenLispVector(value.GetRange(start, value.Count - start));
+        }
+    }
+}
