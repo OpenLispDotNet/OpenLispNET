f12284f4ebde2f65205a8e475aca7a48e3faed4a
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 00:42:58 -0400

    Refactoring ToString() to an expression.

--- OpenLisp.Core/DataTypes/OpenLispString.cs
+++ OpenLisp.Core/DataTypes/OpenLispString.cs
@@ -18,15 +18,12 @@ namespace OpenLisp.Core.DataTypes
             return this;
         }
 
-        public override string ToString()
-        {
-            return "\"" + _value + "\"";
-        }
+        public override string ToString() => "\"" + _value + "\"";
 
         public override string ToString(bool printReadably)
         {
             if (_value.Length > 0 && _value[0] == '\u029e')
-            {
+            {   
                 return ":" + _value.Substring(1);
             }
             if (printReadably)
