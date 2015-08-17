ff55c34725ecf2fdd1abfe9dee31baeab6f4f4e4
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 00:38:25 -0400

    Adding OpenLispString class.

---
+++ OpenLisp.Core/DataTypes/OpenLispString.cs
@@ -0,0 +1,41 @@
+﻿using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispString : OpenLispVal
+    {
+        private string _value;
+
+        public string Value => _value;
+
+        public OpenLispString(string value)
+        {
+            _value = value;
+        }
+
+        public new OpenLispString Copy()
+        {
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return "\"" + _value + "\"";
+        }
+
+        public override string ToString(bool printReadably)
+        {
+            if (_value.Length > 0 && _value[0] == '\u029e')
+            {
+                return ":" + _value.Substring(1);
+            }
+            if (printReadably)
+            {
+                return "\"" + _value.Replace("\\", "\\\\")
+                    .Replace("\"", "\\\"")
+                    .Replace("\n", "\\n") + "\"";
+            }
+            return _value;
+        }
+    }
+}
---
+++ OpenLisp.Core/DataTypes/OpenLispSymbol.cs
@@ -0,0 +1,36 @@
+﻿using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispSymbol : OpenLispVal
+    {
+        private string _value;
+
+        public string Value => _value;
+
+        public OpenLispSymbol(string value)
+        {
+            _value = value;
+        }
+
+        public OpenLispSymbol(OpenLispString value)
+        {
+            _value = value.Value;
+        }
+
+        public new OpenLispSymbol Copy()
+        {
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        public override string ToString(bool printReadably)
+        {
+            return _value;
+        }
+    }
+}
