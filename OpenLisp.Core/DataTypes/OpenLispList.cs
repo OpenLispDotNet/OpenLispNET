d338188214f382b88f051dba9c9e70e48f24341c
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 01:49:33 -0400

    Fleshing out OpenLispList class.

--- OpenLisp.Core/DataTypes/OpenLispList.cs
+++ OpenLisp.Core/DataTypes/OpenLispList.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.StaticClasses;
 
 namespace OpenLisp.Core.DataTypes
 {
@@ -13,6 +14,8 @@ namespace OpenLisp.Core.DataTypes
         private List<OpenLispVal> _value;
 
         public List<OpenLispVal> Value => _value;
+
+        public int Size => _value.Count;
 
         public override bool ListQ()
         {
@@ -50,7 +53,34 @@ namespace OpenLisp.Core.DataTypes
 
         public override string ToString()
         {
-            return Start + printer.
+            return Start + Printer.Join(_value, " ", true) + End;
+        }
+
+        public override string ToString(bool printReadably)
+        {
+            return Start + Printer.Join(_value, " ", printReadably) + End;
+        }
+
+        public OpenLispVal Nth(int index)
+        {
+            return _value.Count > index ? _value[index] : StaticOpenLispTypes.Nil;
+        }
+
+        public OpenLispVal this[int index] => _value.Count > index ? _value[index] : StaticOpenLispTypes.Nil;
+
+        public OpenLispList Rest()
+        {
+            return Size > 0 ? new OpenLispList(_value.GetRange(1, _value.Count - 1)) : new OpenLispList();
+        }
+
+        public virtual OpenLispList Slice(int start)
+        {
+            return new OpenLispList(_value.GetRange(start, _value.Count - 1));
+        }
+
+        public virtual OpenLispList Slice(int start, int end)
+        {
+            return new OpenLispList(_value.GetRange(start, end - start));
         }
     }
 }
