af6674b05e661ee6c65885930f6c1251c32b1e29
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 01:34:09 -0400

    Adding Printer static class.

---
+++ OpenLisp.Core/DataTypes/OpenLispList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispList : OpenLispVal
+    {
+        public string Start = "(";
+
+        public string End = ")";
+
+        private List<OpenLispVal> _value;
+
+        public List<OpenLispVal> Value => _value;
+
+        public override bool ListQ()
+        {
+            return true;
+        }
+
+        public OpenLispList()
+        {
+            _value = new List<OpenLispVal>();
+        }
+
+        public OpenLispList(List<OpenLispVal> value)
+        {
+            _value = value;
+        }
+
+        public OpenLispList(params OpenLispVal[] values)
+        {
+            _value = new List<OpenLispVal>();
+
+            Conj(values);
+        }
+
+        public OpenLispList Conj(params OpenLispVal[] values)
+        {
+            //foreach (var v in values)
+            //{
+            //    _value.Add(v);
+            //}
+
+            _value.AddRange(values.ToList());
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Start + printer.
+        }
+    }
+}
---
+++ OpenLisp.Core/StaticClasses/Printer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.DataTypes;
+
+namespace OpenLisp.Core.StaticClasses
+{
+    public static class Printer
+    {
+        public static string Join(IList<OpenLispVal> values, string delim, bool printReadably)
+        {
+            return String.Join(delim, values.Select(v => v.ToString(printReadably)).ToArray());
+        }
+
+        public static string Join(IDictionary<string, OpenLispVal> values, string delim, bool printReadably)
+        {
+            List<string> strings = new List<string>();
+
+            foreach (KeyValuePair<string, OpenLispVal> v in values)
+            {
+                if (v.Key.Length > 0 && v.Key[0] == '\u029e')
+                {
+                    strings.Add(":" + v.Key.Substring(1));
+                }
+                else if (printReadably)
+                {
+                    strings.Add("\"" + v.Key + "\"");
+                }
+                else
+                {
+                    strings.Add(v.Key);
+                }
+
+                strings.Add(v.Value.ToString(printReadably));
+            }
+
+            return String.Join(delim, strings.ToArray());
+        }
+
+        public static string PrStr(OpenLispVal value, bool printReadably)
+        {
+            return value.ToString(printReadably);
+        }
+
+        public static string PrStrArgs(OpenLispList args, String separator, bool printReadably)
+        {
+            return Join(args.Value, separator, printReadably);
+        }
+
+        public static string EscapeString(string str)
+        {
+            return Regex.Escape(str);
+        }
+    }
+}
