728cfd59f4661b4e7fb05a756212bf3c417d56d4
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 13:16:24 -0400

    Adding Env class.

---
+++ OpenLisp.Core/DataTypes/OpenLispFunc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispFunc : OpenLispVal
+    {
+        private Func<OpenLispList, OpenLispVal> _lambda = null;
+
+        private OpenLispVal _ast = null;
+    }
+}
---
+++ OpenLisp.Core/Env.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.DataTypes;
+using OpenLisp.Core.DataTypes.Errors.Throwable;
+
+namespace OpenLisp.Core
+{
+    public class Env
+    {
+        private Env _outer = null;
+
+        private Dictionary<string, OpenLispVal> _data = new Dictionary<string, OpenLispVal>();
+
+        public Env(Env outer)
+        {
+            _outer = outer;
+        }
+
+        public Env(Env outer, OpenLispList binds, OpenLispList expressions)
+        {
+            _outer = outer;
+
+            for (int i = 0; i < binds.Size; i++)
+            {
+                string symbol = ((OpenLispSymbol) binds.Nth(i)).Value;
+
+                if (symbol == "&")
+                {
+                    _data[((OpenLispSymbol) binds.Nth(i + 1)).Value] = expressions.Slice(i);
+                    break;
+                }
+
+                _data[symbol] = expressions.Nth(i);
+            }
+        }
+
+        public Env Find(OpenLispSymbol key)
+        {
+            return _data.ContainsKey(key.Value) ? this : _outer?.Find(key);
+        }
+
+        public OpenLispVal Get(OpenLispSymbol key)
+        {
+            Env e = Find(key);
+
+            if (e == null)
+            {
+                throw new OpenLispException("'" + key.Value + "' not found");
+            }
+            return e._data[key.Value];
+        }
+
+        public Env Set(OpenLispSymbol key, OpenLispVal value)
+        {
+            _data[key.Value] = value;
+
+            return this;
+        }
+    }
+}
