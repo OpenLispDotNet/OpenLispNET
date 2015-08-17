6229866f21f65a77e676d8ec664d95a00c591ca2
Ramon Long <ramonjlongiii@gmail.com>
2015-08-17 12:41:29 -0400

    Adding OpenLispAtom class.

---
+++ OpenLisp.Core/DataTypes/OpenLispAtom.cs
@@ -0,0 +1,30 @@
+﻿using OpenLisp.Core.AbstractClasses;
+
+namespace OpenLisp.Core.DataTypes
+{
+    public class OpenLispAtom : OpenLispVal
+    {
+        private OpenLispVal _value;
+
+        public OpenLispVal Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public OpenLispAtom(OpenLispVal value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return "(atom " + StaticClasses.Printer.PrStr(_value, true) + ")";
+        }
+
+        public override string ToString(bool printReadably)
+        {
+            return "(atom " + StaticClasses.Printer.PrStr(_value, printReadably) + ")";
+        }
+    }
+}
