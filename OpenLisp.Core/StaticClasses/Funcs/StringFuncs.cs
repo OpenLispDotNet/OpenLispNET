97880d5219a66f4fdcdc0b01d17e93cc5cc30e00
Ramon Long <ramonjlongiii@gmail.com>
2015-08-18 23:06:38 -0400

    Working on StringFunc static lambdas.

---
+++ OpenLisp.Core/StaticClasses/Funcs/StringFuncs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenLisp.Core.DataTypes;
+
+namespace OpenLisp.Core.StaticClasses.Funcs
+{
+    public class StringFuncs
+    {
+        public static OpenLispFunc PrStr = new OpenLispFunc(x =>
+            new OpenLispString(Printer.PrStrArgs(x, " ", true)));
+
+        public static OpenLispFunc Str = new OpenLispFunc(x => 
+            new OpenLispString(Printer.PrStrArgs(x, " ", false)));
+
+        public static OpenLispFunc Prn = new OpenLispFunc(x =>
+        {
+            Console.WriteLine(Printer.PrStrArgs(x, " ", true));
+            return StaticOpenLispTypes.Nil;
+        });
+
+        public static OpenLispFunc PrintLn = new OpenLispFunc(x =>
+        {
+            Console.WriteLine(Printer.PrStrArgs(x, " ", false));
+            return StaticOpenLispTypes.Nil;
+        });
+
+        public static OpenLispFunc OpenLispReadLine = new OpenLispFunc(x =>
+        {
+            var line = readline.
+        });
+    }
+}
