362cbf6eb93a711984ede4954f0b68c477dba16f
Ramon Long <ramonjlongiii@gmail.com>
2015-08-18 22:45:32 -0400

    Adding NumberFuncs class with static TimeMs lambda.

---
+++ OpenLisp.Core/StaticClasses/Funcs/NumberFuncs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OpenLisp.Core.DataTypes;
+
+namespace OpenLisp.Core.StaticClasses.Funcs
+{
+    public class NumberFuncs
+    {
+         public static OpenLispFunc TimeMs = new OpenLispFunc(x =>
+            new OpenLispInt(DateTime.Now.Ticks / 10000L));
+    }
+}
