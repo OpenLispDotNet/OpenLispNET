b17a87c156a1fed238c19c6d62434b6c2778df85
Ramon Long <ramonjlongiii@gmail.com>
2015-08-25 12:48:41 -0400

    Adding Metadata Funcs.

---
+++ OpenLisp.Core/StaticClasses/Funcs/MetadataFuncs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.DataTypes;
+using OpenLisp.Core.DataTypes.Errors.Throwable;
+
+namespace OpenLisp.Core.StaticClasses.Funcs
+{
+    public class MetadataFuncs
+    {
+        public static OpenLispFunc Meta = new OpenLispFunc(x => x[0].Meta);
+
+        public static OpenLispFunc WithMeta = new OpenLispFunc(x => x[0].Copy().Meta = x[1]);
+    }
+}
