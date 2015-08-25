c6c9a46a0583fda3b1b8e354299257b915945498
Ramon Long <ramonjlongiii@gmail.com>
2015-08-25 12:43:01 -0400

    Addign Sequence and List Funcs.

---
+++ OpenLisp.Core/StaticClasses/Funcs/ListFuncs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.DataTypes;
+using OpenLisp.Core.DataTypes.Errors.Throwable;
+
+namespace OpenLisp.Core.StaticClasses.Funcs
+{
+    public class ListFuncs
+    {
+        public static OpenLispFunc Apply = new OpenLispFunc(x =>
+        {
+            var f = (OpenLispFunc)x[0];
+            var dataList = new List<OpenLispVal>();
+
+            dataList.AddRange(x.Slice(1, x.Size - 1).Value);
+            dataList.AddRange(((OpenLispList)x[x.Size - 1]).Value);
+
+            return f.Apply(new OpenLispList(dataList));
+        });
+
+        public static OpenLispFunc Map = new OpenLispFunc(x =>
+        {
+            OpenLispFunc f = (OpenLispFunc)x[0];
+            var sourceList = ((OpenLispList)x[1]).Value;
+            var newList = sourceList.Select(t => f.Apply(new OpenLispList(t))).ToList();
+
+            return new OpenLispList(newList);
+        });
+    }
+}
--- OpenLisp.Core/StaticClasses/Funcs/SequenceFuncs.cs
+++ OpenLisp.Core/StaticClasses/Funcs/SequenceFuncs.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using OpenLisp.Core.AbstractClasses;
 using OpenLisp.Core.DataTypes;
 using OpenLisp.Core.DataTypes.Errors.Throwable;
@@ -15,12 +16,11 @@ namespace OpenLisp.Core.StaticClasses.Funcs
 
         public static OpenLispFunc Cons = new OpenLispFunc(x =>
         {
-            var listData = new List<OpenLispVal>();
+            var listData = new List<OpenLispVal> {x[0]};
 
-            listData.Add(x[0]);
             listData.AddRange(((OpenLispList) x[1]).Value);
 
-            return (OpenLispVal) new OpenLispList(listData);
+            return new OpenLispList(listData);
         });
 
         public static OpenLispFunc Concat = new OpenLispFunc(x =>
@@ -40,7 +40,53 @@ namespace OpenLisp.Core.StaticClasses.Funcs
 
         public static OpenLispFunc Nth = new OpenLispFunc(x =>
         {
-            
+            var index = (int)((OpenLispInt)x[1]).Value;
+
+            if (index < ((OpenLispList) x[0]).Size)
+            {
+                return ((OpenLispList) x[0])[index];
+            }
+
+            throw new OpenLispException($"Nth: index: {index} out of range: {((OpenLispList) x[0]).Size}");
+        });
+
+        public static OpenLispFunc First = new OpenLispFunc(x => ((OpenLispList)x[0])[0]);
+
+        public static OpenLispFunc Rest = new OpenLispFunc(x => ((OpenLispList)x[0]).Rest());
+
+        public static OpenLispFunc EmptyQ = new OpenLispFunc(x =>
+            ((OpenLispList) x[0]).Size == 0
+                ? StaticOpenLispTypes.True
+                : StaticOpenLispTypes.False);
+
+        public static OpenLispFunc Count = new OpenLispFunc(x => 
+            (x[0] == StaticOpenLispTypes.Nil)
+                ? new OpenLispInt(0)
+                : new OpenLispInt(((OpenLispList)x[0]).Size));
+
+        public static OpenLispFunc Conj = new OpenLispFunc(x =>
+        {
+            var sourceList = ((OpenLispList)x[0]).Value;
+            var newList = new List<OpenLispVal>();
+
+            newList.AddRange(sourceList);
+
+            if (x[0] is OpenLispVector)
+            {
+                for (int i = 1; i < x.Size; i++)
+                {
+                    newList.Add(x[i]);
+                }
+
+                return new OpenLispVector(newList);
+            }
+
+            for (int i = 1; i < x.Size; i++)
+            {
+                newList.Insert(0, x[i]);
+            }
+
+            return new OpenLispList(newList);
         });
     }
 }
