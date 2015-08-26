b6027f948561f1c78dc4916f0536218a47ebc7d9
Ramon Long <ramonjlongiii@gmail.com>
2015-08-25 21:21:50 -0400

    Working on StaticOpenLispTypes static class.

--- OpenLisp.Core/StaticClasses/StaticOpenLispTypes.cs
+++ OpenLisp.Core/StaticClasses/StaticOpenLispTypes.cs
@@ -26,46 +26,40 @@ namespace OpenLisp.Core.StaticClasses
             Type typeB = b.GetType();
 
 
-            if ((typeA == typeB) || (a is OpenLispList && b is OpenLispList))
-            {
-                if (a is OpenLispInt)
-                {
-                    return ((OpenLispInt) a).Value ==
-                           ((OpenLispInt) b).Value;
-                }
-                else if (a is OpenLispSymbol)
-                {
-                    return ((OpenLispSymbol) a).Value ==
-                           ((OpenLispSymbol) b).Value;
-                }
-                else if (a is OpenLispString)
-                {
-                    return ((OpenLispString) a).Value ==
-                           ((OpenLispString) b).Value;
-                }
-                else if (a is OpenLispList)
-                {
-                    if (((OpenLispList) a).Size != ((OpenLispList) b).Size)
-                    {
-                        return false;
-                    }
-                    for (int i = 0; i < ((OpenLispList) a).Size; i++)
-                    {
-                        if (!StaticOpenLispTypes.OpenLispEqualQ(((OpenLispList) a)[i], ((OpenLispList) b)[i]))
-                        {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-                else
-                {
-                    return a == b;
-                }
-            }
-            //return (typeA == typeB) || (a is OpenLispList && b is OpenLispList);
+            if ((typeA != typeB) && (!(a is OpenLispList) || !(b is OpenLispList))) return false;
 
-            return false;
+            if (a is OpenLispInt)
+            {
+                return ((OpenLispInt) a).Value ==
+                       ((OpenLispInt) b).Value;
+            }
+
+            if (a is OpenLispSymbol)
+            {
+                return ((OpenLispSymbol) a).Value ==
+                       ((OpenLispSymbol) b).Value;
+            }
+
+            if (a is OpenLispString)
+            {
+                return ((OpenLispString) a).Value ==
+                       ((OpenLispString) b).Value;
+            }
+
+            if (!(a is OpenLispList)) return a == b;
+
+            if (((OpenLispList) a).Size != ((OpenLispList) b).Size) return false;
+
+            for (int i = 0; i < ((OpenLispList) a).Size; i++)
+            {
+                if (StaticOpenLispTypes.OpenLispEqualQ(((OpenLispList) a)[i], ((OpenLispList) b)[i]))
+                    continue;
+
+                return false;
+            }
+
+            return true;
+            //return (typeA == typeB) || (a is OpenLispList && b is OpenLispList);
         }
 
         public static OpenLispConstant Nil = new OpenLispConstant("nil");
