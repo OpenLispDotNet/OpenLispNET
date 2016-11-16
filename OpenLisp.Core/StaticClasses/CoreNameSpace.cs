0b93bcee9910dab397eb2ddc2bb0e4da9e4aae5b
Ramon J. Long III <ramonjlongiii@gmail.com>
2016-11-16 14:41:33 -0800

    Refactoring to use double-check locking singletons so that the core NameSpace is easily extensible and overridable.

--- OpenLisp.Core/StaticClasses/CoreNameSpace.cs
+++ OpenLisp.Core/StaticClasses/CoreNameSpace.cs
@@ -17,95 +17,170 @@ namespace OpenLisp.Core.StaticClasses
      * 
      * To extend the core Ns, simply add new elements to the IDictionary.
      */
-    public static partial class CoreNameSpace
+    public partial class CoreNameSpace
     {
-        public static IDictionary<string, OpenLispVal> Ns = new Dictionary<string, OpenLispVal>
+        private static volatile CoreNameSpace _instance;
+        private static object _syncRoot = new object();
+
+        private CoreNameSpace()
         {
-#if !NOTYPEEQUALITY
-            {"=",           new OpenLispFunc(x => 
-                                StaticOpenLispTypes.OpenLispEqualQ(x[0], x[1]) 
-                                    ? StaticOpenLispTypes.True 
-                                    : StaticOpenLispTypes.False)},
-#elif NOTYPEEQUALITY
-            {"=",           new OpenLispFunc(x => 
-                                (x[0].ToString() == x[1].ToString())
-                                    ? StaticOpenLispTypes.True
-                                    : StaticOpenLispTypes.False)},
-#endif
 
-            {"throw",       ThrowFuncs.OpenLispThrow},
+        }
 
-            {"nil?",        ScalarFuncs.NilQ},
-            {"true?",       ScalarFuncs.TrueQ},
-            {"false?",      ScalarFuncs.FalseQ},
-            {"symbol",      ScalarFuncs.Symbol},
-            {"symbol?",     ScalarFuncs.SymbolQ},
-            {"keyword",     ScalarFuncs.Keyword},
-            {"keyword?",    ScalarFuncs.KeywordQ},
+        /// <summary>
+        /// Uses double-check locking to retrieve a CoreNameSpace singleton.
+        /// 
+        /// Under most circumstances, this should be thread-safe.
+        /// </summary>
+        public static CoreNameSpace Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new CoreNameSpace();
+                        }
+                    }
+                }
 
-            {"pr-str",      StringFuncs.PrStr},
-            {"str",         StringFuncs.Str},
-            {"prn",         StringFuncs.Prn},
-            {"println",     StringFuncs.PrintLn},
-            {"readline",    StringFuncs.OpenLispReadLine},
-            {"read-string", StringFuncs.ReadString},
-            {"slurp",       StringFuncs.Slurp},
+                return _instance;
+            }
+        }
 
-            {"<",           new OpenLispFunc(x => 
-                                (OpenLispInt)x[0] < (OpenLispInt)x[1])},
-            {"<=",          new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] <= (OpenLispInt)x[1])},
-            {">",           new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] > (OpenLispInt)x[1])},
-            {">=",          new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] >= (OpenLispInt)x[1])},
-            {"+",           new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] + (OpenLispInt)x[1])},
-            {"-",           new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] - (OpenLispInt)x[1])},
-            {"*",           new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] * (OpenLispInt)x[1])},
-            {"/",           new OpenLispFunc(x =>
-                                (OpenLispInt)x[0] / (OpenLispInt)x[1])},
+        private static volatile IDictionary<string, OpenLispVal> _defaultNameSpaceInstance;
+        private static object _nsGetLock = new object();
+        private static object _nsSetLock = new object();
 
-            {"time-ms",     NumberFuncs.TimeMs},
+        /// <summary>
+        /// Contains the singleton of the Ns object.
+        /// 
+        /// Before setting the value of this property, you must first
+        /// get the default instance.
+        /// </summary>
+        public static IDictionary<string, OpenLispVal> Ns
+        {
+            get
+            {
+                if (_defaultNameSpaceInstance == null)
+                {
+                    lock (_nsGetLock)
+                    {
+                        if (_defaultNameSpaceInstance == null)
+                        {
+                            #region Default Name Space
+                            _defaultNameSpaceInstance = new Dictionary<string, OpenLispVal>
+                            {
+                    #if !NOTYPEEQUALITY
+                                {"=",           new OpenLispFunc(x => 
+                                                    StaticOpenLispTypes.OpenLispEqualQ(x[0], x[1]) 
+                                                        ? StaticOpenLispTypes.True 
+                                                        : StaticOpenLispTypes.False)},
+                    #elif NOTYPEEQUALITY
+                                {"=",           new OpenLispFunc(x =>
+                                                    (x[0].ToString() == x[1].ToString())
+                                                        ? StaticOpenLispTypes.True
+                                                        : StaticOpenLispTypes.False)},
+                    #endif
 
-            {"list",        new OpenLispFunc(x => new OpenLispList(x.Value))},
-            {"list?",       CollectionFuncs.ListQ},
+                                {"throw",       ThrowFuncs.OpenLispThrow},
 
-            {"vector",      new OpenLispFunc(x => new OpenLispVector(x.Value))},
-            {"vector?",     CollectionFuncs.VectorQ},
+                                {"nil?",        ScalarFuncs.NilQ},
+                                {"true?",       ScalarFuncs.TrueQ},
+                                {"false?",      ScalarFuncs.FalseQ},
+                                {"symbol",      ScalarFuncs.Symbol},
+                                {"symbol?",     ScalarFuncs.SymbolQ},
+                                {"keyword",     ScalarFuncs.Keyword},
+                                {"keyword?",    ScalarFuncs.KeywordQ},
 
-            {"hash-map",    new OpenLispFunc(x => new OpenLispHashMap(x))},
-            {"map?",        HashMapFuncs.HashMapQ},
-            {"contains?",   HashMapFuncs.ContainsQ},
-            {"assoc",       HashMapFuncs.Assoc},
-            {"dissoc",      HashMapFuncs.Dissoc},
-            {"get",         HashMapFuncs.Get},
-            {"keys",        HashMapFuncs.Keys},
-            {"vals",        HashMapFuncs.Values},
+                                {"pr-str",      StringFuncs.PrStr},
+                                {"str",         StringFuncs.Str},
+                                {"prn",         StringFuncs.Prn},
+                                {"println",     StringFuncs.PrintLn},
+                                {"readline",    StringFuncs.OpenLispReadLine},
+                                {"read-string", StringFuncs.ReadString},
+                                {"slurp",       StringFuncs.Slurp},
 
-            {"sequential?", SequenceFuncs.SequentialQ},
-            {"cons",        SequenceFuncs.Cons},
-            {"concat",      SequenceFuncs.Concat},
-            {"nth",         SequenceFuncs.Nth},
-            {"first",       SequenceFuncs.First},
-            {"rest",        SequenceFuncs.Rest},
-            {"empty?",      SequenceFuncs.EmptyQ},
-            {"count",       SequenceFuncs.Count},
-            {"conj",        SequenceFuncs.Conj},
+                                {"<",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] < (OpenLispInt)x[1])},
+                                {"<=",          new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] <= (OpenLispInt)x[1])},
+                                {">",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] > (OpenLispInt)x[1])},
+                                {">=",          new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] >= (OpenLispInt)x[1])},
+                                {"+",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] + (OpenLispInt)x[1])},
+                                {"-",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] - (OpenLispInt)x[1])},
+                                {"*",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] * (OpenLispInt)x[1])},
+                                {"/",           new OpenLispFunc(x =>
+                                                    (OpenLispInt)x[0] / (OpenLispInt)x[1])},
 
-            {"apply",       ListFuncs.Apply},
-            {"map",         ListFuncs.Map},
+                                {"time-ms",     NumberFuncs.TimeMs},
 
-            {"with-meta",   MetadataFuncs.WithMeta},
-            {"meta",        MetadataFuncs.Meta},
+                                {"list",        new OpenLispFunc(x => new OpenLispList(x.Value))},
+                                {"list?",       CollectionFuncs.ListQ},
 
-            {"atom",        new OpenLispFunc(x => new OpenLispAtom(x[0]))},
-            {"atom?",       AtomFuncs.AtomQ},
-            {"deref",       AtomFuncs.Deref},
-            {"reset!",      AtomFuncs.ResetBang},
-            {"swap!",       AtomFuncs.SwapBang}
-        };
+                                {"vector",      new OpenLispFunc(x => new OpenLispVector(x.Value))},
+                                {"vector?",     CollectionFuncs.VectorQ},
+
+                                {"hash-map",    new OpenLispFunc(x => new OpenLispHashMap(x))},
+                                {"map?",        HashMapFuncs.HashMapQ},
+                                {"contains?",   HashMapFuncs.ContainsQ},
+                                {"assoc",       HashMapFuncs.Assoc},
+                                {"dissoc",      HashMapFuncs.Dissoc},
+                                {"get",         HashMapFuncs.Get},
+                                {"keys",        HashMapFuncs.Keys},
+                                {"vals",        HashMapFuncs.Values},
+
+                                {"sequential?", SequenceFuncs.SequentialQ},
+                                {"cons",        SequenceFuncs.Cons},
+                                {"concat",      SequenceFuncs.Concat},
+                                {"nth",         SequenceFuncs.Nth},
+                                {"first",       SequenceFuncs.First},
+                                {"rest",        SequenceFuncs.Rest},
+                                {"empty?",      SequenceFuncs.EmptyQ},
+                                {"count",       SequenceFuncs.Count},
+                                {"conj",        SequenceFuncs.Conj},
+
+                                {"apply",       ListFuncs.Apply},
+                                {"map",         ListFuncs.Map},
+
+                                {"with-meta",   MetadataFuncs.WithMeta},
+                                {"meta",        MetadataFuncs.Meta},
+
+                                {"atom",        new OpenLispFunc(x => new OpenLispAtom(x[0]))},
+                                {"atom?",       AtomFuncs.AtomQ},
+                                {"deref",       AtomFuncs.Deref},
+                                {"reset!",      AtomFuncs.ResetBang},
+                                {"swap!",       AtomFuncs.SwapBang}
+                            };
+#endregion
+                        }
+                    }
+                }
+
+                return _defaultNameSpaceInstance;
+            }
+
+            set
+            {
+                if (_defaultNameSpaceInstance != null)
+                {
+                    lock (_nsSetLock)
+                    {
+                        if (_defaultNameSpaceInstance != null)
+                        {
+                            _defaultNameSpaceInstance = value;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
