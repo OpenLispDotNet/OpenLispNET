0dc8bfee2b7c96622a0e9f0655c5a5e92cb9aa60
Ramon J. Long III <ramonjlongiii@gmail.com>
2016-12-22 13:04:01 -0800

    Adding documentation to Env.

--- OpenLisp.Core/Env.cs
+++ OpenLisp.Core/Env.cs
@@ -5,17 +5,30 @@ using OpenLisp.Core.DataTypes.Errors.Throwable;
 
 namespace OpenLisp.Core
 {
+    /// <summary>
+    /// Manages the OpenLisp.NET environment containing OpenLispSymbol and OpenLispVal objects.
+    /// </summary>
     public class Env
     {
         private readonly Env _outer = null;
 
         private readonly Dictionary<string, OpenLispVal> _data = new Dictionary<string, OpenLispVal>();
 
+        /// <summary>
+        /// Constructor accepting an outer <see cref="Env"/>.
+        /// </summary>
+        /// <param name="outer"><see cref="Env"/></param>
         public Env(Env outer)
         {
             _outer = outer;
         }
 
+        /// <summary>
+        /// Constructor accepting an outer <see cref="Env"/> with binds and expressions.
+        /// </summary>
+        /// <param name="outer"><see cref="Env"/></param>
+        /// <param name="binds"><see cref="OpenLispList"/></param>
+        /// <param name="expressions"><see cref="OpenLispList"/></param>
         public Env(Env outer, OpenLispList binds, OpenLispList expressions)
         {
             _outer = outer;
@@ -34,11 +47,21 @@ namespace OpenLisp.Core
             }
         }
 
+        /// <summary>
+        /// Finds an <see cref="OpenLispSymbol"/> in <see cref="Env._data"/>.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public Env Find(OpenLispSymbol key)
         {
             return _data.ContainsKey(key.Value) ? this : _outer?.Find(key);
         }
 
+        /// <summary>
+        /// Gets an <see cref="OpenLispSymbol"/> from <see cref="Env"/>.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public OpenLispVal Get(OpenLispSymbol key)
         {
             Env e = Find(key);
@@ -50,6 +73,12 @@ namespace OpenLisp.Core
             return e._data[key.Value];
         }
 
+        /// <summary>
+        /// Sets an <see cref="OpenLispSymbol"/> key's value to an instance of <see cref="OpenLispVal"/>.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public Env Set(OpenLispSymbol key, OpenLispVal value)
         {
             _data[key.Value] = value;
@@ -57,6 +86,10 @@ namespace OpenLisp.Core
             return this;
         }
 
+        /// <summary>
+        /// Return the name of the Type when <see cref="Env.ToString"/> is invoked.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{GetType().Name}";
