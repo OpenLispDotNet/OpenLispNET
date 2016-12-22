4cb4f268523cc4b6071cca071a75d61a52304c46
Ramon J. Long III <ramonjlongiii@gmail.com>
2016-12-21 23:15:31 -0800

    Working on OpenLisp.NET language extensions c/o ArcanaERP.

---
+++ OpenLisp.Core/Interfaces/IOpenLispMonad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLisp.Core.Interfaces
+{
+    public interface IOpenLispMonad<T>
+    {
+        IOpenLispMonad<TO> Bind<TO>(Func<T, IOpenLispMonad<TO>> func);
+    }
+}
