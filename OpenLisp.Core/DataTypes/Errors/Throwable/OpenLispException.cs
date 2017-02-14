a599662bed3db87d0fccfd3e8ad8332166f3508d
Ramon J. Long III <ramonjlongiii@gmail.com>
2017-02-13 22:59:54 -0800

    Adding privately mutable _dateTimeStamp to help trace exceptionhistories.

--- OpenLisp.Core/DataTypes/Errors/Throwable/OpenLispException.cs
+++ OpenLisp.Core/DataTypes/Errors/Throwable/OpenLispException.cs
@@ -1,11 +1,34 @@
-﻿namespace OpenLisp.Core.DataTypes.Errors.Throwable
+﻿using System;
+
+namespace OpenLisp.Core.DataTypes.Errors.Throwable
 {
     /// <summary>
     /// An exception that can be thrown by OpenLisp.NET
     /// </summary>
     public class OpenLispException : OpenLispThrowable
     {
+        private DateTime _dateTimeStamp;
         private object _value;
+
+        /// <summary>
+        /// Private constructor to initialize the privately
+        /// mutable _dateTimeStamp and assign <see cref="DateTime.Now"/>.
+        /// 
+        /// <seealso cref="OpenLispException.DateTimeStamp"/>
+        /// </summary>
+        private OpenLispException()
+        {
+            _dateTimeStamp = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Get the <see cref="DateTime"/> Stamp at
+        /// the time of Exception.  This field should never
+        /// be mutable by an external client.  Precise
+        /// DateTime values are useful for tracing and auditing
+        /// execution paths.
+        /// </summary>
+        public DateTime DateTimeStamp => _dateTimeStamp;
 
         /// <summary>
         /// The value of the <see cref="OpenLispException"/>
