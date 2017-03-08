46d7862d4a55f7b1a20dc40de4494fa0dda47c69
Ramon J. Long III <ramonjlongiii@gmail.com>
2017-03-07 23:00:43 -0800

    Working on a generic Monad interface for OpenLisp.Net

--- OpenLisp.Core/Interfaces/IOpenLispMonad.cs
+++ OpenLisp.Core/Interfaces/IOpenLispMonad.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenLisp.Core.DataTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,47 @@ namespace OpenLisp.Core.Interfaces
 {
     /// <summary>
     /// Interface to describe an implementation of a monad in OpenLisp.NET
+    /// 
+    /// Any implementation of this interface should be considered the Monadic type.
+    /// Any typeparam passed in as type T should be consider the Monadic Data type.
+    /// 
+    /// https://en.wikipedia.org/wiki/Monad_(functional_programming)#Formal_definition
     /// </summary>
-    /// <typeparam name="T">The Type whose behavior you want to bind in your monad.</typeparam>
+    /// <typeparam name="T">The Data Type you want to bind in your monad.</typeparam>
     public interface IOpenLispMonad<T>
     {
         /// <summary>
         /// Bind to <see cref="IOpenLispMonad{TO}"/> using a <see cref="Func{T, TResult}"/>
+        /// 
+        /// Formally: 
+        /// 
+        ///     (M t) -> (t -> M u) -> (M u)
+        /// 
+        /// TODO: represent with the ">>=" to correspond to the familiar binding operation of Haskell.
         /// </summary>
-        /// <typeparam name="TO">The Type bound to in your monad.</typeparam>
+        /// <typeparam name="U">The Data Type bound to in your monad.</typeparam>
         /// <param name="func">The binding function of your monad.</param>
         /// <returns></returns>
-        IOpenLispMonad<TO> Bind<TO>(Func<T, IOpenLispMonad<TO>> func);
+        IOpenLispMonad<U> Bind<U>(Func<T, IOpenLispMonad<U>> func);
+
+        /// <summary>
+        /// A type constructor that defines, for every underlying type, how to obtain a corresponding monadic type.
+        /// </summary>
+        /// <param name="openLispFunc"></param>
+        /// <param name="environment"></param>
+        /// <returns></returns>
+        IOpenLispMonad<T> TypeConstructor(OpenLispFunc openLispFunc, Env environment);
+
+        /// <summary>
+        /// Injects a value in an underlying type to a value in the 
+        /// corresponding monadic type.
+        /// 
+        /// Formally:
+        /// 
+        ///     t -> M t
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IOpenLispMonad<T> UnitFunction(T dataType);
     }
 }
