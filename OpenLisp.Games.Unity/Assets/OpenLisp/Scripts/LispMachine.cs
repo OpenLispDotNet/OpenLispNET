7450f59049d7ad898549d41007663c3c05d9c48e
wizardbeard <ramonjlongiii@gmail.com>
2024-03-31 20:59:30 -0700

    feat: iterating on OpenLisp.Games.Unity.SDK

--- OpenLisp.Games.Unity/Assets/OpenLisp/Scripts/LispMachine.cs
+++ OpenLisp.Games.Unity/Assets/OpenLisp/Scripts/LispMachine.cs
@@ -1,23 +1,22 @@
-//using OpenLisp.Core;
+using OpenLisp.Core;
 using UnityEngine;
 
-namespace Assets.OpenLisp.Scripts
+
+
+public class LispMachine : MonoBehaviour
 {
+    public Env env = new Env(null);
 
-    public class LispMachine : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        //public Env env = new Env(null);
 
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
+
