69f686a926b27e7df01d0bc0e4d2db64730b16de
wizardbeard <ramonjlongiii@gmail.com>
2024-03-31 18:44:07 -0700

    wip: OpenLisp.Games.Unity.SDK

---
+++ OpenLisp.Games.Unity.SDK/LispMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenLisp.Core;
+using OpenLisp.Core.AbstractClasses;
+using OpenLisp.Core.StaticClasses;
+using UnityEngine;
+
+namespace OpenLisp.Games.Unity.SDK
+{
+    public class LispMachine : MonoBehaviour
+    {
+
+        private static readonly Env LispEnv = new Env(null);
+        private Func<string, OpenLispVal> ReadEval = (string str) => Repl.Eval(Repl.Read(str), LispEnv);
+        public GameObject Input { get; set; }
+    }
+}
--- OpenLisp.Games.Unity.SDK/Properties/AssemblyInfo.cs
+++ OpenLisp.Games.Unity.SDK/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OpenLisp.Games.Unity.SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Wizard & The Wyrd, LLC")]
 [assembly: AssemblyProduct("OpenLisp.Games.Unity.SDK")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
