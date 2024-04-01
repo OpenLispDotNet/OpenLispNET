39d2373b5a99c4e9897457cc6f1b22745e18d65a
wizardbeard <ramonjlongiii@gmail.com>
2024-03-31 17:31:44 -0700

    wip: OpenLisp.Games.Unity

--- OpenLisp.Games.Unity/Assets/OpenLisp/Scripts/LispMachine.cs
+++ OpenLisp.Games.Unity/Assets/OpenLisp/Scripts/LispMachine.cs
@@ -1,9 +1,13 @@
+using OpenLisp.Core;
 using UnityEngine;
 
 namespace Assets.OpenLisp.Scripts
 {
+
     public class LispMachine : MonoBehaviour
     {
+        public Env env = new Env(null);
+
         // Start is called before the first frame update
         void Start()
         {
