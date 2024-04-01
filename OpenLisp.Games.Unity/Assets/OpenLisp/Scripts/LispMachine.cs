b7113ad16b1bb87355c6ddf52e35c6813c23073d
wizardbeard <ramonjlongiii@gmail.com>
2024-03-31 17:09:12 -0700

    wip: Unity integration

---
+++ OpenLisp.Games.Unity/Assets/OpenLisp/Scripts/LispMachine.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Assets.OpenLisp.Scripts
+{
+    public class LispMachine : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
